6b82be3993fbd200c9f88bfb13b047a837260183
Wolfogan <wolfogan@gmail.com>
2014-04-28 20:00:26 -0700

    Renglon de inicio a tablas en ventanas de los catálogos

--- public/js/ventanamodal.js
+++ public/js/ventanamodal.js
@@ -2,7 +2,7 @@
 var titulos = {
 	'plan' :{ 
 		'titulo':'Número de Plan',
-		'campos':['Clave','Nombre','Fecha Inicio','Fecha Final','Eliminar']
+		'campos':['Clave','Nombre','Fecha Inicio','Fecha Final']
 	},
 	'unidad':{
 		'titulo':'Unidad Acádemica',
@@ -42,7 +42,13 @@ var titulos = {
 	}
 };
 
-
+/**
+ * Función que crea la estructura de las ventanas modales con el contenido que es una tabla.
+ * @param  {string} id     Indica el id que tendra la ventana modal en el catálogo.
+ * @param  {string} titulo Título que despliega la tabla modal.
+ * @param  {string} tabla  HTML de la tabla para embeberla.
+ * @return {string}        Retorna la ventana completa en formato HTML.
+ */
 function estructura(id,titulo,tabla)
 {
 	var ventana ='<div class="md-modal md-effect-11" id="'+ id +'"> ' +
@@ -56,13 +62,36 @@ function estructura(id,titulo,tabla)
 	return ventana;
 }
 
+
+function crearTabla(encabezados)
+{
+	var tabla = ' <table width="90%" id="modal1"><thead>';
+	var renglon = '';
+	tabla += "<tr>";
+	for(var i in encabezados)
+	{
+		tabla += "<th>" + encabezados[i] + "</th>";
+		renglon += '<td>';
+		renglon += '<input type="text" style:"width:5px;" />';
+		renglon += '</td>';
+	}
+	// Agregar la columna eliminar registro.
+	tabla+='<th>Eliminar</th>';
+
+	// Agregar al renglón de inicio en la columna eliminar un botón.
+	renglon+='<td width="50" align="center"><input type="button" value="-" class="clsEliminarFila"/></td>';
+	
+	tabla += '</tr></thead><tbody><tr>'+ renglon +'</tr></tbody></table>';
+	return tabla;
+}
+
 function crearCatalogos()
 {
 	var ventanaCatalogos='';
 
-	for(var idKey in titulos)
+	for(var id in titulos)
 	{
-		ventanaCatalogos += estructura(idKey,titulos[idKey].titulo,crearTablas(titulos[idKey].campos));
+		ventanaCatalogos += estructura(id,titulos[id].titulo,crearTabla(titulos[id].campos));
 	}
 	ventanaCatalogos += '<div class="md-overlay"></div>';
 	
@@ -71,17 +100,7 @@ function crearCatalogos()
 
 
 
-function crearTablas(encabezados)
-{
-	var table = ' <table width="100%" id="modal1"><thead>';
-	table += "<tr>";
-	for(var i in encabezados)
-	{
-		table += "<th>" + encabezados[i] + "</th>";
-	}
-	table += '</tr></thead><tbody></tbody></table>';
-	return table;
-}
+
 
 /*$(function(){
 	$(document).on('click','.clsAgregarFilaNoPlan',function(){
@@ -97,4 +116,37 @@ function crearTablas(encabezados)
 
 		var objTabla=$(this).parents().get(0);
 		$(objTabla).find('table').find('tbody').append(strNueva_Fila);
-});*/
+
+
+});
+
+//cuando se haga clic en cualquier clase .clsEliminarFila se dispara el evento
+	$(document).on('click','.clsEliminarFila',function(){
+		/*if(!confirm('¿Desea eliminar el registro?')){
+					return;
+				}*/
+		/*obtener el cuerpo de la tabla; contamos cuantas filas (tr) tiene
+		si queda solamente una fila le preguntamos al usuario si desea eliminarla
+		var objCuerpo=$(this).parents().get(2);
+			if($(objCuerpo).find('tr').length==1){
+				if(!confirm('Esta es la única fila de la lista ¿Desea eliminarla?')){
+					return;
+				}
+			}
+					
+		/*obtenemos el padre (tr) del td que contiene a nuestro boton de eliminar
+		que quede claro: estamos obteniendo dos padres
+					
+		el asunto de los padres e hijos funciona exactamente como en la vida real
+		es una jergarquia. imagine un arbol genealogico y tendra todo claro ;)
+				
+			tr	--> padre del td que contiene el boton
+				td	--> hijo de tr y padre del boton
+					boton --> hijo directo de td (y nieto de tr? si!)
+		
+		var objFila=$(this).parents().get(1);
+			/*eliminamos el tr que contiene los datos del contacto (se elimina todo el
+			contenido (en este caso los td, los text y logicamente, el boton 
+			$(objFila).remove();
+	});
+*/
