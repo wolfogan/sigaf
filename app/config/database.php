5eee32111757f444a77a36e08cc57dfa974c7bc6
tyntia <zyntyaduarte@hotmail.com>
2014-08-19 17:06:20 -0700

    cambios

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'sigaf',
+			'database'  => 'sigaf_i',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
