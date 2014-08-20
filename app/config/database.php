4515c8fb9f1f4918daca809d970bee619211478a
tyntia <zyntyaduarte@hotmail.com>
2014-08-19 17:11:45 -0700

    Revert "cambios"
    This reverts commit 5eee32111757f444a77a36e08cc57dfa974c7bc6.

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'sigaf_i',
+			'database'  => 'sigaf',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
